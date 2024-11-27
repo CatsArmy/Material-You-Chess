328ba793122da7acc1fae6d2ff9aef1f2bdb6c2b
CatsArmy <bergman.itai@gmail.com>
2024-11-27 14:42:21 +0200

    quick fix

--- App2/ChessBoard/Knight.cs
+++ App2/ChessBoard/Knight.cs
@@ -7,6 +7,12 @@ public class Knight : Piece
 {
     public Knight(ImageView piece, int id, ImageView space, bool isWhite, int spaceId) : base(piece, id, space, isWhite, spaceId) { }
 
+    public override List<Move> Moves(Dictionary<(char, int), Space> board, Dictionary<(string, int), Piece> pieces)
+    {
+        List<Move> moves = new List<Move>();
+        return moves;
+    }
+
     public override bool Move(Space dest, Dictionary<(char, int), Space> board, Dictionary<(string, int), Piece> pieces)
     {
         bool isLegalMove = true;
